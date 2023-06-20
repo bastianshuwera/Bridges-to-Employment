a1c6029ba9b5566f6ee76e0fbc080006fc87889d
bastianshuwera <136705576+bastianshuwera@users.noreply.github.com>
2023-06-20 16:22:31 +0700

    Add automated tests

---
+++ unicorn_mountain_apple.vb
@@ -0,0 +1,115 @@
+Module Module1
+
+Sub Main()
+    Dim strProgramName As String = "Bridges To Employment"
+    Console.WriteLine("Welcome to " & strProgramName & " Program")
+    Console.WriteLine("************************")
+    Dim strName As String 
+    Console.WriteLine("Please enter your name: ")
+    strName = Console.ReadLine()
+    Console.WriteLine("Hello " & strName )
+    Dim intAge As Integer
+    Console.WriteLine("Please enter your age: ")
+    intAge = Console.ReadLine()
+    Console.WriteLine("Your age is " & intAge )
+    Dim strEducation As String 
+    Console.WriteLine("Please enter your highest level of education: ")
+    strEducation = Console.ReadLine()
+    Console.WriteLine("Your highest level of education is " & strEducation )
+    Dim strJobType As String
+    Console.WriteLine("Please enter your desired job type: ")
+    strJobType = Console.ReadLine()
+    Console.WriteLine("Your desired job type is " & strJobType )
+    Dim intYearsExperience As Integer
+    Console.WriteLine("Please enter your years of experience: ")
+    intYearsExperience = Console.ReadLine()
+    Console.WriteLine("Your years of experience is " & intYearsExperience )
+    Dim intMonthlyIncome As Integer
+    Console.WriteLine("Please enter your desired monthly income: ")
+    intMonthlyIncome = Console.ReadLine()
+    Console.WriteLine("Your desired monthly income is " & intMonthlyIncome )
+    Dim strJobSearch As String
+    Console.WriteLine("How are you searching for jobs (e.g. online, newspapers, etc): ")
+    strJobSearch = Console.ReadLine()
+    Console.WriteLine("You are searching for jobs in the following manner: " & strJobSearch )
+    Dim intTotalJobsApplied As Integer
+    Console.WriteLine("Please enter your total number of jobs applied: ")
+    intTotalJobsApplied = Console.ReadLine()
+    Console.WriteLine("Your total number of jobs applied is " & intTotalJobsApplied )
+    Dim intInterviewsAttended As Integer
+    Console.WriteLine("Please enter your total number of interviews attended: ")
+    intInterviewsAttended = Console.ReadLine()
+    Console.WriteLine("Your total number of interviews attended is " & intInterviewsAttended )
+    Dim intPositionsOffered As Integer
+    Console.WriteLine("Please enter your total number of positions offered: ")
+    intPositionsOffered = Console.ReadLine()
+    Console.WriteLine("Your total number of positions offered is " & intPositionsOffered )
+    Dim strResumeStatus As String
+    Console.WriteLine("What is the status of your resume (e.g. updated, outdated): ")
+    strResumeStatus = Console.ReadLine()
+    Console.WriteLine("Your resume status is " & strResumeStatus )
+    Dim strEmploymentStatus As String 
+    Console.WriteLine("What is your current employment status (e.g. employed, unemployed): ")
+    strEmploymentStatus = Console.ReadLine()
+    Console.WriteLine("Your current employment status is " & strEmploymentStatus )
+    Dim strEmploymentGap As String 
+    Console.WriteLine("Are you experienced any employment gaps: ")
+    strEmploymentGap = Console.ReadLine()
+    Console.WriteLine("You are experienced the following employment gaps: " & strEmploymentGap )
+    Dim strVolunteerExperience As String 
+    Console.WriteLine("Please enter your volunteer experience, if any: ")
+    strVolunteerExperience = Console.ReadLine()
+    Console.WriteLine("Your volunteer experience is " & strVolunteerExperience )
+    Dim strProfessionalMemberships As String 
+    Console.WriteLine("Please enter your professional memberships, if any: ")
+    strProfessionalMemberships = Console.ReadLine()
+    Console.WriteLine("Your professional memberships is " & strProfessionalMemberships )
+    Dim strReferences As String 
+    Console.WriteLine("Please list any references you may have: ")
+    strReferences = Console.ReadLine()
+    Console.WriteLine("Your references are: " & strReferences )
+    Dim strAwards As String 
+    Console.WriteLine("Please enter any awards or recognition received: ")
+    strAwards = Console.ReadLine()
+    Console.WriteLine("Your awards and recognition are: " & strAwards )
+    Dim strNetworking As String
+    Console.WriteLine("Please enter any methods of networking (e.g. online, in person): ")
+    strNetworking = Console.ReadLine()
+    Console.WriteLine("Your networking methods are: " & strNetworking )
+    Dim strMentoring As String 
+    Console.WriteLine("Do you have a mentor or group of mentors you work with: ")
+    strMentoring = Console.ReadLine()
+    Console.WriteLine("You are in mentorship with the following people: " & strMentoring )
+    Dim strGoals As String 
+    Console.WriteLine("Please enter any immediate employment goals you may: ")
+    strGoals = Console.ReadLine()
+    Console.WriteLine("Your immediate employment goals are: " & strGoals )
+    Dim strSkills As String
+    Console.WriteLine("Please list skills or qualifications you may have: ")
+    strSkills = Console.ReadLine()
+    Console.WriteLine("Your skills and qualifications are: " & strSkills )
+    Dim strComputerSkills As String
+    Console.WriteLine("Please enter your computer skills: ")
+    strComputerSkills = Console.ReadLine()
+    Console.WriteLine("Your computer skills are: " & strComputerSkills )
+    Dim strWorkExperience As String
+    Console.WriteLine("Please enter your work experience: ")
+    strWorkExperience = Console.ReadLine()
+    Console.WriteLine("Your work experience is: " & strWorkExperience )
+    Dim strSalaryExpectations As String
+    Console.WriteLine("Please enter your salary expectations: ")
+    strSalaryExpectations = Console.ReadLine()
+    Console.WriteLine("Your salary expectations are: " & strSalaryExpectations )
+    Dim strCareerInterests As String
+    Console.WriteLine("Please enter your career interests: ")
+    strCareerInterests = Console.ReadLine()
+    Console.WriteLine("Your career interests are: " & strCareerInterests )
+    Dim strCertifications As String
+    Console.WriteLine("Please enter any certifications or other qualifications you may have: ")
+    strCertifications = Console.ReadLine()
+    Console.WriteLine("Your certifications and qualifications are: " & strCertifications )
+    Console.WriteLine("Thank you for completing the " & strProgramName & " Program")
+    Console.WriteLine("************************")
+End Sub
+
+End Module
