8bd3babf3f244fa59a8ff5b7dcabf18d972a5566
bastianshuwera <136705576+bastianshuwera@users.noreply.github.com>
2023-07-17 12:14:15 +0700

    Update reporting mechanisms

---
+++ apple_piano_house.kt
@@ -0,0 +1,434 @@
+fun main() {
+
+    //1. Create a function to explore potential employers
+    fun exploreEmployers(location: String) {
+        println("Exploring potential employers located in $location")
+    }
+
+    //2. Create a function to generate a list of companies
+    fun generateCompanyList(numberOfCompanies: Int): List<String> {
+        val companies = mutableListOf<String>()
+        for (i in 0 until numberOfCompanies) {
+            companies.add("Company $i")
+        }
+        return companies
+    }
+
+    //3. Create a function to apply to a company
+    fun applyToCompany(companyName: String) {
+        println("Applying to $companyName")
+    }
+
+    //4. Create a function to search job postings
+    fun searchJobPostings(keywords: String) {
+        println("Searching for jobs with keywords: $keywords")
+    }
+
+    //5. Create a function to prepare for an interview
+    fun prepareForInterview(skill: String) {
+        println("Preparing for an interview by practicing $skill")
+    }
+
+    //6. Create a function to attend an interview
+    fun attendInterview(location: String) {
+        println("Attending an interview at $location")
+    }
+
+    //7. Create a function to follow up after an interview
+    fun followUpAfterInterview(date: String) {
+        println("Following up after an interview on $date")
+    }
+
+    //8. Create a function to land a job
+    fun landJob(companyName: String) {
+        println("Landing a job at $companyName")
+    }
+
+    //9. Create a function to begin the job
+    fun beginJob(location: String) {
+        println("Starting a new job at $location")
+    }
+
+    //10. Create a function to track job progress
+    fun trackProgress(goal: String) {
+        println("Tracking progress towards achieving $goal")
+    }
+
+    //11. Create a function to network with other professionals
+    fun networkWithProfessionals(profession: String) {
+        println("Networking with other $profession professionals")
+    }
+
+    //12. Create a function to attend conferences
+    fun attendConferences(conferenceName: String) {
+        println("Attending $conferenceName conferences")
+    }
+
+    //13. Create a function to explore career growth opportunities
+    fun exploreCareerGrowthOpportunities(interests: String) {
+        println("Exploring career growth opportunities related to $interests")
+    }
+
+    //14. Create a function to take courses
+    fun takeCourses(courseName: String) {
+        println("Taking $courseName courses")
+    }
+
+    //15. Create a function to connect with alumni
+    fun connectWithAlumni(university: String) {
+        println("Connecting with alumni from $university")
+    }
+
+    //16. Create a function to join a professional society
+    fun joinProfessionalSociety(societyName: String) {
+        println("Joining the $societyName professional society")
+    }
+
+    //17. Create a function to participate in community activities
+    fun participateInCommunityActivities(activity: String) {
+        println("Participating in the $activity community activity")
+    }
+
+    //18. Create a function to gain experience
+    fun gainExperience(skill: String) {
+        println("Gaining experience in $skill")
+    }
+
+    //19. Create a function to develop relationships
+    fun developRelationships(person: String) {
+        println("Developing a relationship with $person")
+    }
+
+    //20. Create a function to update your portfolio
+    fun updatePortfolio(projectName: String) {
+        println("Updating portfolio with $projectName project")
+    }
+
+    //21. Create a function to find opportunities
+    fun findOpportunities(location: String) {
+        println("Finding opportunities in $location")
+    }
+
+    //22. Create a function to write cover letters
+    fun writeCoverLetters(template: String) {
+        println("Writing cover letters using $template template")
+    }
+
+    //23. Create a function to stay organized
+    fun stayOrganized() {
+        println("Staying organized by creating a plan and tracking progress")
+    }
+
+    //24. Create a function to create a resume
+    fun createResume(format: String) {
+        println("Creating a resume in the $format format")
+    }
+
+    //25. Create a function to build connections
+    fun buildConnections(person: String) {
+        println("Building connections with $person")
+    }
+
+    //26. Create a function to review job postings
+    fun reviewJobPostings(posting: String) {
+        println("Reviewing the $posting job posting")
+    }
+
+    //27. Create a function to create an elevator pitch
+    fun createElevatorPitch(topic: String) {
+        println("Creating an elevator pitch about $topic")
+    }
+
+    //28. Create a function to research the company
+    fun researchCompany(companyName: String) {
+        println("Researching $companyName company")
+    }
+
+    //29. Create a function to apply to job postings
+    fun applyToJobPostings(posting: String) {
+        println("Applying to the $posting job posting")
+    }
+
+    //30. Create a function to practice interviewing
+    fun practiceInterviewing(skill: String) {
+        println("Practicing $skill interviewing techniques")
+    }
+
+    //31. Create a function to assess interests
+    fun assessInterests() {
+        println("Assessing interests to identify potential career paths")
+    }
+
+    //32. Create a function to update social media profiles
+    fun updateSocialMediaProfiles(platform: String) {
+        println("Updating $platform social media profile")
+    }
+
+    //33. Create a function to review job descriptions
+    fun reviewJobDescriptions(description: String) {
+        println("Reviewing the $description job description")
+    }
+
+    //34. Create a function to ask for feedback
+    fun askForFeedback(person: String) {
+        println("Asking $person for feedback on job search")
+    }
+
+    //35. Create a function to create a job alert
+    fun createJobAlert(searchTerm: String) {
+        println("Creating a job alert for $searchTerm")
+    }
+
+    //36. Create a function to read industry news
+    fun readIndustryNews(publication: String) {
+        println("Reading $publication industry news")
+    }
+
+    //37. Create a function to use job search engines
+    fun useJobSearchEngines(engine: String) {
+        println("Using $engine job search engine")
+    }
+
+    //38. Create a function to build a professional network
+    fun buildProfessionalNetwork(person: String) {
+        println("Building a professional network with $person")
+    }
+
+    //39. Create a function to reach out to recruiters
+    fun reachOutToRecruiters(contact: String) {
+        println("Reaching out to $contact recruiter")
+    }
+
+    //40. Create a function to attend networking events
+    fun attendNetworkingEvents(eventName: String) {
+        println("Attending $eventName networking event")
+    }
+
+    //41. Create a function to use job boards
+    fun useJobBoards(boardName: String) {
+        println("Using $boardName job board")
+    }
+
+    //42. Create a function to practice communication techniques
+    fun practiceCommunicationTechniques(skill: String) {
+        println("Practicing $skill communication techniques")
+    }
+
+    //43. Create a function to review job postings
+    fun reviewJobPosting(posting: String) {
+        println("Reviewing the $posting job posting")
+    }
+
+    //44. Create a function to attend workshops
+    fun attendWorkshops(workshopName: String) {
+        println("Attending the $workshopName workshop")
+    }
+
+    //45. Create a function to volunteer
+    fun volunteer(organization: String) {
+        println("Volunteering at $organization")
+    }
+
+    //46. Create a function to take assessments
+    fun takeAssessments(assessmentType: String) {
+        println("Taking $assessmentType assessments")
+    }
+
+    //47. Create a function to ask for introductions
+    fun askForIntroductions(person: String) {
+        println("Asking $person for introductions")
+    }
+
+    //48. Create a function to use LinkedIn
+    fun useLinkedIn() {
+        println("Using LinkedIn to find potential employers")
+    }
+
+    //49. Create a function to keep resumes up to date
+    fun keepResumesUpToDate() {
+        println("Keeping resumes up to date with current job search")
+    }
+
+    //50. Create a function to attend a job fair
+    fun attendJobFair(location: String) {
+        println("Attending a job fair in $location")
+    }
+
+    //51. Create a function to join job boards
+    fun joinJobBoards(boardName: String) {
+        println("Joining the $boardName job board")
+    }
+
+    //52. Create a function to research job roles
+    fun researchJobRoles(role: String) {
+        println("Researching $role job roles")
+    }
+
+    //53. Create a function to practice presentation skills
+    fun practicePresentationSkills(skill: String) {
+        println("Practicing $skill presentation skills")
+    }
+
+    //54. Create a function to create goals
+    fun createGoals(goalType: String) {
+        println("Creating $goalType goals")
+    }
+
+    //55. Create a function to develop skills
+    fun developSkills(skill: String) {
+        println("Developing $skill skills")
+    }
+
+    //56. Create a function to practice problem solving
+    fun practiceProblemSolving(topic: String) {
+        println("Practicing problem solving related to $topic")
+    }
+
+    //57. Create a function to stay motivated
+    fun stayMotivated() {
+        println("Staying motivated by setting daily goals and tracking progress")
+    }
+
+    //58. Create a function to participate in contests
+    fun participateInContests(contestName: String) {
+        println("Participating in the $contestName contest")
+    }
+
+    //59. Create a function to update job search materials
+    fun updateJobSearchMaterials() {
+        println("Updating job search materials with the most current information")
+    }
+
+    //60. Create a function to shadow a professional
+    fun shadowProfessional(name: String) {
+        println("Shadowing $name professional")
+    }
+
+    //61. Create a function to explore internship programs
+    fun exploreInternshipPrograms(field: String) {
+        println("Exploring internship programs in $field")
+    }
+
+    //62. Create a function to research salary ranges
+    fun researchSalaryRanges(location: String) {
+        println("Researching salary ranges for $location")
+    }
+
+    //63. Create a function to learn new technologies
+    fun learnNewTechnologies(technology: String) {
+        println("Learning the $technology technology")
+    }
+
+    //64. Create a function to share success stories
+    fun shareSuccessStories(story: String) {
+        println("Sharing the $story success story")
+    }
+
+    //65. Create a function to find mentors
+    fun findMentors(interests: String) {
+        println("Finding mentors with similar $interests interests")
+    }
+
+    //66. Create a function to keep a record of contacts
+    fun keepRecordOfContacts() {
+        println("Keeping a record of contacts and networking relationships")
+    }
+
+    //67. Create a function to learn about company culture
+    fun learnAboutCompanyCulture(companyName: String) {
+        println("Learning about the $companyName company culture")
+    }
+
+    //68. Create a function to use online job search tools
+    fun useOnlineJobSearchTools(toolName: String) {
+        println("Using the $toolName online job search tool")
+    }
+
+    //69. Create a function to read industry trends
+    fun readIndustryTrends(topic: String) {
+        println("Reading industry trends related to $topic")
+    }
+
+    //70. Create a function to reach out to a contact
+    fun reachOutToContact(person: String) {
+        println("Reaching out to $person contact")
+    }
+
+    //71. Create a function to ask questions
+    fun askQuestions(topic: String) {
+        println("Asking questions related to $topic")
+    }
+
+    //72. Create a function to create a career plan
+    fun createCareerPlan() {
+        println("Creating a career plan to track progress")
+    }
+
+    //73. Create a function to look for job openings
+    fun lookForJobOpenings(location: String) {
+        println("Looking for job openings in $location")
+    }
+
+    //74. Create a function to take advantage of resources
+    fun takeAdvantageOfResources(resourceName: String) {
+        println("Taking advantage of $resourceName resources")
+    }
+
+    //75. Create a function to review success stories
+    fun reviewSuccessStories(story: String) {
+        println("Reviewing the $story success story")
+    }
+
+    //76. Create a function to practice communication strategies
+    fun practiceCommunicationStrategies(strategy: String) {
+        println("Practicing $strategy communication strategies")
+    }
+
+    //77. Create a function to follow blogs
+    fun followBlogs(blogName: String) {
+        println("Following the $blogName blog")
+    }
+
+    //78. Create a function to build a website
+    fun buildWebsite(siteName: String) {
+        println("Building the $siteName website")
+    }
+
+    //79. Create a function to review job postings
+    fun reviewJobPost(posting: String) {
+        println("Reviewing the $posting job posting")
+    }
+
+    //80. Create a function to write a thank you letter
+    fun writeThankYouLetter(recipient: String) {
+        println("Writing a thank you letter to $recipient")
+    }
+
+    //81. Create a function to join online communities
+    fun joinOnlineCommunities(communityName: String) {
+        println("Joining the $communityName online community")
+    }
+
+    //82. Create a function to attend seminars
+    fun attendSeminars(seminarName: String) {
+        println("Attending the $seminarName seminar")
+    }
+
+    //83. Create a function to practice interview questions
+    fun practiceInterviewQuestions(topic: String) {
+        println("Practicing interview questions related to $topic")
+    }
+
+    //84. Create a function to research companies
+    fun researchCompanies(companyName: String) {
+        println("Researching the $companyName company")
+    }
+
+    //85. Create a function to acquire new skills
+    fun acquireNewSkills(skill: String) {
+        println("Acquiring new $skill skills")
+    }
+
+    //86. Create a function to use job boards
+    fun useJobBoard(boardName: String) {
+        println("
